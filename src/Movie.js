5e11018173b7f8d95fc339449c58219e983457c9
ceppetto <pcy07111@gmail.com>
2021-06-25 17:39:40 +0900

    Add flex display

--- src/Movie.js
+++ src/Movie.js
@@ -2,13 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css"
 
-function Movie({id, year, title, summary, poster}) {
+function Movie({ id, year, title, summary, poster, genres }) {
     return(
         <div className="movies__movie">
-            <img src={poster} alt={title} title={title}></img>
+            <div className="movie__img">
+                <img src={poster} alt={title} title={title}></img>
+            </div>
             <div className="movie__data">
-                <h3 className="movie__title">{title}</h3>
-                <h5 className="movie__year">{year}</h5>
+                <h3 className="movie__title" >{title} ({year})</h3>
+                <p className="genres">{genres}</p>
                 <p className="movie__summary">{summary}</p>
             </div>
         </div>
@@ -20,7 +22,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
