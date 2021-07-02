42c5a16e6ece8416dfa775d0a8595d1740901917
ceppetto <pcy07111@gmail.com>
2021-07-02 17:54:41 +0900

    Fix navigation style

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./Movie.css"
 
 function Movie({ id, year, title, summary, poster, genres, ytcode }) {
     return(
+        <Link
+        to={{
+            pathname: "/movie-detail",
+            state: {
+                year, title, summary, poster, genres
+            }
+        }}>
         <div className="movies__movie">
             <img src={poster} alt={title} title={title}></img>
             <div className="movie__data">
@@ -13,6 +21,7 @@ function Movie({ id, year, title, summary, poster, genres, ytcode }) {
             </div>
             
         </div>
+        </Link>
     );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,8 +5,8 @@ import './Navigation.css'
 function Navigation() {
     return (
         <div>
-            <div><Link to="/">Home</Link></div>
-            <div><Link to="/about">About</Link></div>
+            <div className="nav"><Link to="/">Home</Link></div>
+            <div className="nav"><Link to="/about">About</Link></div>
         </div>
     );
 }
